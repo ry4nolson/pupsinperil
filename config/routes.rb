a7042716a7eee5f43a7efa218c22d28f082e6feb
Ryan Olson <ry4nolson@gmail.com>
2017-12-05 05:37:32 +0000

    kinda handle 404s

--- config/routes.rb
+++ config/routes.rb
@@ -21,4 +21,6 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
+  
+  get '*path' => redirect('/')
 end
